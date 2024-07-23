c6b8cd35b6e600b0b634ad6b49c5db4b71a4ac75
Vyashvardhan <yvvijay28@gmail.com>
2024-07-23 10:22:13 +0530

    final commit

---
+++ src/about/subheadingsData.js
@@ -0,0 +1,107 @@
+const subheadingsData = {
+  1: [
+    {
+      title: "About Me",
+      content: (
+        <>
+          <p>
+            Energetic and passionate college student working towards B.Tech in
+            Computer Science and Communication at Manipal Institute of
+            Technology. Aiming to us the technology acquired through my course
+            work, self study and with my hard work to explore the fun world of
+            Software Engineer.
+          </p>
+        </>
+      ),
+    },
+  ],
+  2: [
+    {
+      title: "Ongoing Bachelors Degree",
+      content: (
+        <>
+          <p>
+            <li>
+              A degree in MIT, Manipal in Computer and Communication Engineering
+              with minor specialisation and Data Analysis.
+            </li>
+            <p></p>
+            <li>
+              A core comitee member of the 'Manipal Open Source Society' and
+              'The Data Alchemists'.
+            </li>
+            <p></p>
+            <li>A member of the dance crew of "The Show Stoppers".</li>
+          </p>
+        </>
+      ),
+    },
+    {
+      title: "2021 High School",
+      content: (
+        <>
+          <p>
+            <li>
+              A highly enthusiastic and free-spirited member of the school band
+              and the dance team representing the school in many competitions.
+            </li>
+          </p>
+        </>
+      ),
+    },
+  ],
+  3: [
+    {
+      title: "Cisco Certified Network Associate",
+      content: (
+        <>
+          <p>
+            <li>
+              Performed data generation via GAN’s for increasing the dataset
+              size and diversity
+            </li>
+            <p></p>
+            <li>
+              Fine tuned GAN models to give better results by encoding different
+              features from different images and generating a new and super
+              realistic image
+            </li>
+            <p></p>
+            <li>
+              Use of zero-shot stable diffusion models and fine tuning the
+              models for desired style image generation.
+            </li>
+          </p>
+        </>
+      ),
+    },
+  ],
+  4: [
+    {
+      title: "EID Intern GE Healthcare Jun'24 - Jul'24",
+      content: (
+        <>
+          <p>
+            <li>
+              Performed data generation via GAN’s for increasing the dataset
+              size and diversity
+            </li>
+            <p></p>
+            <li>
+              Fine tuned GAN models to give better results by encoding different
+              features from different images and generating a new and super
+              realistic image
+            </li>
+            <p></p>
+            <li>
+              Use of zero-shot stable diffusion models and fine tuning the
+              models for desired style image generation.
+            </li>
+          </p>
+        </>
+      ),
+    },
+  ],
+};
+
+export default subheadingsData;
