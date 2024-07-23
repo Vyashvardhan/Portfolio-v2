5dcf6c1681e41a64c2d8d473545f301b32cec845
Vyashvardhan <yvvijay28@gmail.com>
2024-07-23 10:31:56 +0530

    Updated Projects

--- src/projects/projectsData.js
+++ src/projects/projectsData.js
@@ -4,18 +4,15 @@ import projectThree from "../assets/project-3.png";
 
 const projects = {
   1: {
-    title: "SoMe Platform",
+    title: "My Windows: Mindows",
     image: projectOne,
     description: (
       <>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </p>
+        <p>A minimal Windows 10 experience on the browser</p>
       </>
     ),
-    github: "https://github.com",
-    demo: "https://netlify.com",
+    github: "https://github.com/Vyashvardhan/MyWindows-Mindows",
+    demo: "https://mindows.netlify.app",
   },
   2: {
     title: "Productivity App",
